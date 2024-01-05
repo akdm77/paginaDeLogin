b85dffd35067001ed73edacb8ba3f5dc5cc30a9f
alanklysmandiasmacedo <alanklysmandiasmacedo@pop-os.localdomain>
2024-01-05 18:29:24 -0300

    second commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ import validator from 'validator';
 import './globals.css';
 
 
+
 export default function Home() {
 
   
@@ -18,7 +19,7 @@ export default function Home() {
       if(validator.isEmail(email)){
         setEmailError("Email valido")
       }else{
-        setEmailError("Insira um email valido")
+        setEmailError("*Insira um email valido*")
     }}else{
       setEmailError("")
     }
@@ -34,33 +35,32 @@ export default function Home() {
 
   return (
 
-      <main className=" flex  h-screen justify-center items-center align-center  "
-      style={{backgroundImage:`url(${`./img1/R.jpeg`})`}} >
-        <div className="grid backdrop-blur-sm p-10 aligh-center items-center justify-center  w-2/4 rounded-md  " >
-          <h1 className='flex aligh-center justify-center text-lg' >Formulario</h1>
+      <main className=" flex bg-[url('./img/R.jpeg')] bg-center bg-cover h-screen justify-center items-center align-center  ">
+        <div className="grid  backdrop-blur-md p-10 items-center justify-center rounded-lg shadow-md shadow-black  text-white  " >
+          <h1 className='flex text-center items-center pb-6 justify-center text-lg' >Formulario</h1>
         
           <form onSubmit={handleSubmit}>
-            <label className="flex  m-3">
-              <span>Nome: </span>
-              <input className="rounded-md "  value={name}  type="text" placeholder='Digite seu nome...' onChange={(e) => setName(e.target.value)}/>
+            <label className="flex  h-7">
+              <span className="m-3 "> Nome: </span>
+              <input className="flex justify-center items-center pl-3 rounded-md text-black"  value={name}  type="text" placeholder='Digite seu nome...' onChange={(e) => setName(e.target.value)}/>
             </label>
-            {!!name ? name.length < 10 ? <p className="text-red-500 text=xs ml-9">* insira um nome valido *</p> : null : null}
+            {!!name ? name.length < 10 ? <p className="text-red-500 text=md ml-20 font-serif">* insira um nome valido *</p> : null : null}
             <br />
-            <br />
-            <label className="flex  m-3 ">
-              <span>Senha: </span>
-              <input className="rounded-md" value={pin}  type='password' placeholder="Digite a senha..." onChange={(e) => setPin(e.target.value)}/>
+            
+            <label className="flex  h-7 ">
+              <span className="m-3">Senha: </span>
+              <input className="flex pl-3 rounded-md text-black justify-center items-center" value={pin}  type='password' placeholder="Digite a senha..." onChange={(e) => setPin(e.target.value)}/>
             </label>
-            {!!pin ? pin.length < 8 ? <p className="text-red-500 text-xs ml-9 ">* insira uma senha com 8 caracters *</p> : null : null}
+            {!!pin ? pin.length < 8 ? <p className="text-red-500 text-md ml-20 font-serif ">* insira uma senha com 8 caracters *</p> : null : null}
+            
             <br />
-            <br />
-            <label className="flex  m-3">
-              <span>email:  </span>
-              <input className="rounded-md"   name="email" type="email" placeholder="Digite seu email..." onChange={(e) => validateEmail(e)}/>
+            <label className="flex h-7 ">
+              <span className="m-3">email:  </span>
+              <input className="flex justify-center items-center pl-3 rounded-md text-black"   name="email" type="email" placeholder="Digite seu email..." onChange={(e) => validateEmail(e)}/>
             </label>
-            {!!emailError && <p className="text-red-500 text-xs ml-9">{emailError}</p>}
+            {!!emailError && <p className="text-red-500 text-md ml-20 mt-5 font-serif">{emailError}</p>}
             <br />
-            <input className="flex flex-row bg-blue-400 p-1.5 m-3 rounded-md text-white hover:bg-blue-700" type="submit" value="login" />
+            <input className=" bg-blue-500 p-1.5 m-3 rounded-md size-full text-center  text-white hover:bg-blue-700" type="submit" value="login" />
           </form>
         </div>
       </main>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,7 +9,7 @@ const config: Config = {
   theme: {
     extend: {
       backgroundImage: {
-        
+        'img': "url('/src/app/img/R.jpeg')",
       },
     },
   },
